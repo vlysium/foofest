599ab552cf48d983e6b3cb280233e0c6b6885337
”Jonathan <jona6893@stud.kea.dkgit config --global user.name Jonathan Weldongit config --global user.email jona6893@stud.kea.dk>
2022-12-02 13:25:21 +0100

    hamburger menu almost done

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -3,25 +3,66 @@ import Tickets from "../pages/Tickets";
 import Camping from "../pages/Camping";
 //import Sustainability from "../pages/Sustainability";
 import Home from "../pages/Home";
+import "../styles/hamburger.scss"
+import {useState} from "react"
 
 function Hamburger() {
+const [changeIcon, setChangeIcon] = useState(true); 
+
+function sodi(){
+
+}
+
   return (
     <>
-    <div><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-egg" viewBox="0 0 16 16">
-  <path d="M8 15a5 5 0 0 1-5-5c0-1.956.69-4.286 1.742-6.12.524-.913 1.112-1.658 1.704-2.164C7.044 1.206 7.572 1 8 1c.428 0 .956.206 1.554.716.592.506 1.18 1.251 1.704 2.164C12.31 5.714 13 8.044 13 10a5 5 0 0 1-5 5zm0 1a6 6 0 0 0 6-6c0-4.314-3-10-6-10S2 5.686 2 10a6 6 0 0 0 6 6z"/>
-</svg></div>
-      <div>
+      <div className="menuIcon">
+        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="white" class="bi bi-alarm-fill" viewBox="0 0 16 16">
+  <path d="M6 .5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1H9v1.07a7.001 7.001 0 0 1 3.274 12.474l.601.602a.5.5 0 0 1-.707.708l-.746-.746A6.97 6.97 0 0 1 8 16a6.97 6.97 0 0 1-3.422-.892l-.746.746a.5.5 0 0 1-.707-.708l.602-.602A7.001 7.001 0 0 1 7 2.07V1h-.5A.5.5 0 0 1 6 .5zm2.5 5a.5.5 0 0 0-1 0v3.362l-1.429 2.38a.5.5 0 1 0 .858.515l1.5-2.5A.5.5 0 0 0 8.5 9V5.5zM.86 5.387A2.5 2.5 0 1 1 4.387 1.86 8.035 8.035 0 0 0 .86 5.387zM11.613 1.86a2.5 2.5 0 1 1 3.527 3.527 8.035 8.035 0 0 0-3.527-3.527z"/>
+</svg>
+        <button
+          onClick={() =>
+            setChangeIcon(() => {
+              if (changeIcon === true) {
+                return false;
+              }
+              {
+                return true;
+              }
+            })
+          }
+        >
+          {changeIcon ? "☰" : "X"}
+        </button>
+      </div>
       <Router>
-        <nav>
+        <nav className={changeIcon ? "right slideOut" : "left slideOut"}>
           <ul>
             <li>
-              <Link to="/">Home</Link>
+              <Link to="/"></Link>
             </li>
-            <li>
-              <Link to="/tickets">Tickets</Link>
+            <li onClick={() => setChangeIcon(true)}>
+              <Link to="/tickets">TICKETS</Link>
             </li>
-            <li>
-              <Link to="/camping">Camping</Link>
+            <li onClick={() => setChangeIcon(true)}>
+              <Link to="/camping">CAMPING</Link>
+            </li>
+            <li onClick={() => setChangeIcon(true)}>
+              <a>PROGRAM</a>
+            </li>
+            <li onClick={() => setChangeIcon(true)}>
+              <a>LINE-UP</a>
+            </li>
+            <li onClick={() => setChangeIcon(true)}>
+              <a>ABOUT</a>
+            </li>
+            <li onClick={() => setChangeIcon(true)}>
+              <a>CONTACT</a>
+            </li>
+            <li onClick={() => setChangeIcon(true)}>
+              <a>NEWS</a>
+            </li>
+            <li onClick={() => setChangeIcon(true)}>
+              <a>SUSTAINABILITY</a>
             </li>
           </ul>
         </nav>
@@ -32,7 +73,6 @@ function Hamburger() {
           <Route path="/" element={<Home />}></Route>
         </Routes>
       </Router>
-      </div>
     </>
   );
 }
