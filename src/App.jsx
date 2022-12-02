a3a5af36230f9cc32974b11ee0f2b69c16e7d3f3
”Jonathan <jona6893@stud.kea.dkgit config --global user.name Jonathan Weldongit config --global user.email jona6893@stud.kea.dk>
2022-12-02 11:48:39 +0100

    Change the menu and home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,9 @@
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
-import Tickets from "./pages/Tickets";
-import Camping from "./pages/Camping";
-//import Sustainability from "../pages/Sustainability";
+import Hamburger from "./components/Hamburger";
 import Home from "./pages/Home";
-
 function App() {
   return (
     <>
-      <Router>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/tickets">Tickets</Link>
-            </li>
-            <li>
-              <Link to="/camping">Camping</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Routes>
-          <Route path="/tickets" element={<Tickets />}></Route>
-          <Route path="/camping" element={<Camping />}></Route>
-          <Route path="/" element={<Home />}></Route>
-        </Routes>
-      </Router>
+    <Hamburger/>
     </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>
-        <h1></h1>
-        <p>sadiofuhasopfiuhasioufhaisufhasopifuhaspfiouhspfiouhasipf</p>
-    </div>
+   <>
+   <h1>sodifjvsødoifvøodasifv</h1>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione dolores vitae odit ab possimus animi harum, excepturi minus laborum corrupti dolor illum eius beatae a itaque veniam id quae necessitatibus porro soluta cum non tempore culpa sed? Officiis, rerum eius reiciendis consectetur minima maiores aliquam et, ut error aliquid porro!</p>
+   </>
   )
 }
 
