2b39743b3d6fe2a65bba861a6f92b2eefdde714f
rebe7917 <rebe7917@stud.kea.dk>
2022-12-06 20:40:19 +0100

    campin info site

---
+++ src/camping/GreenCamping.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function GreenCamping() {
+  return (
+    <section>
+      <h2>GREEN CAMPING</h2>
+      <div>
+        <p>
+          At FooFest, we are committed to creating a sustainable and environmentally-friendly festival. To that end, we are proud to offer green camping options for our attendees. Our green camping options include solar-powered showers, compostable
+          toilets, and solar-powered lighting. We also encourage campers to reduce their carbon footprint by bringing reusable water bottles and bags, and utilizing public transportation to get to the festival. With our green camping options, you can
+          enjoy the festival while minimizing your impact on the environment.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default GreenCamping;
