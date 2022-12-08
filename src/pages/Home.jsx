b0328a1bda744a8457da1a6aa1dbe018a48fd1a7
Victor Ly <vict4222@stud.kea.dk>
2022-12-08 09:30:09 +0100

    change anchor tag to link tag

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,18 +10,21 @@ function Home() {
     about: (
       <p>
         Welcome to FooFest! <br />
-        <br /> We are an annual music festival that celebrates the best in music and entertainment. Our mission is to create a fun, safe, and memorable experience for everyone. We feature national and international acts, as well as smaller,
-        up-and-coming acts.
+        <br /> We are an annual music festival that celebrates the best in music and entertainment. Our mission is to
+        create a fun, safe, and memorable experience for everyone. We feature national and international acts, as well
+        as smaller, up-and-coming acts.
         <br />
-        <br /> We strive to provide a safe, welcoming environment and to reduce our carbon footprint. We also have plenty of other attractions including food and drinks, interactive activities, and more!
+        <br /> We strive to provide a safe, welcoming environment and to reduce our carbon footprint. We also have
+        plenty of other attractions including food and drinks, interactive activities, and more!
         <br />
         <br /> Join us for an unforgettable experience!
       </p>
     ),
     camping: (
       <p>
-        Camping at FooFest offers great experiences – take advantage of the atmosphere, make friends, and enjoy the festival. We have 5 camping areas around the festival square, each with food, small shops, toilets, showers, and interactive
-        activities. Add camping to your festival ticket for just 99,- for even more benefits!
+        Camping at FooFest offers great experiences – take advantage of the atmosphere, make friends, and enjoy the
+        festival. We have 5 camping areas around the festival square, each with food, small shops, toilets, showers, and
+        interactive activities. Add camping to your festival ticket for just 99,- for even more benefits!
       </p>
     ),
   };
@@ -31,13 +34,11 @@ function Home() {
   return (
     <>
       <section id="first-section">
-      
-          {/* <h1>Welcome To FOOFEST</h1> */}
-          <Countdown targetDate={oneHour} />
-          <Link to="/tickets">
-            <button>Buy Now</button>
-          </Link>
-       
+        {/* <h1>Welcome To FOOFEST</h1> */}
+        <Countdown targetDate={oneHour} />
+        <Link to="/tickets">
+          <button>Buy Now</button>
+        </Link>
       </section>
       <QuickAbout
         text={ShortText.about}
@@ -51,9 +52,9 @@ function Home() {
       <AboutCamping
         text={ShortText.camping}
         button={
-          <a href="/Camping">
+          <Link to="/Camping">
             <button>Read More</button>
-          </a>
+          </Link>
         }
       />
     </>
