cf4917e1fc0e6d0973029f464f83a5013a568c59
Victor Ly <vict4222@stud.kea.dk>
2022-12-09 12:40:23 +0100

    fixed

--- src/camping/CrewSetUp.jsx
+++ src/camping/CrewSetUp.jsx
@@ -1,25 +1,25 @@
-
-
 function CrewSetUp() {
   return (
-    <section id="second">
-      <div className="about-container">
-        <div className="aboutText">
-          <h2>CREW SETUP</h2>
-          <p>
-            At our festival, we understand that setting up a tent can be a hassle and time consuming. That’s why we offer the unique option of having our experienced festival workers set up your tent for you. Our team will arrive at your designated
-            camping area with a pre-pitched tent that has been set up in advance. All you need to do is show up with your sleeping bag, and you’ll be ready to enjoy the festival. if you are going as a group buy your tickets together then your tents
-            will be put up next to each other as a camp. All tents are made with durable materials and come with a waterproof rainfly. We strive to make your festival experience as hassle-free as possible. By having our experienced festival workers
-            set up your tent for you, you can focus on enjoying the music and activities.
-          </p>
+    <section id="crew-setup">
+      <div className="crew-setup-container">
+        <div className="text-wrapper">
+          <div className="crew-setup-text">
+            <h2>CREW SETUP</h2>
+            <p>
+              At our festival, we understand that setting up a tent can be a hassle and time consuming. That's why we
+              offer the unique option of having our experienced festival workers set up your tent for you. Our team will
+              arrive at your designated camping area with a pre-pitched tent that has been set up in advance. All you
+              need to do is show up with your sleeping bag, and you'll be ready to enjoy the festival. if you are going
+              as a group buy your tickets together then your tents will be put up next to each other as a camp. All
+              tents are made with durable materials and come with a waterproof rainfly. We strive to make your festival
+              experience as hassle-free as possible. By having our experienced festival workers set up your tent for
+              you, you can focus on enjoying the music and activities.
+            </p>
+          </div>
         </div>
-        <div className="aboutImage aboutImage3"></div>
+        <div className="crew-setup-image"></div>
       </div>
     </section>
-  
-    
-    
-
   );
 }
 
