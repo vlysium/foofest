fde18c7aa4acbad62ab98dfb31651b236fa9b2bc
Victor Ly <vict4222@stud.kea.dk>
2022-12-12 12:13:15 +0100

    fixed id, classes, image

--- src/components/jobs/Job.jsx
+++ src/components/jobs/Job.jsx
@@ -1,22 +1,27 @@
-import React from 'react'
+import React from "react";
 
 function Job() {
   return (
-    <section id="job-sec">
-      <div className="about-container">
-        <div className="aboutText">
-          <h1>WORK AT FOOFEST</h1>
-          <p>
-            Working at a festival is a great way to be part of the action and have a unique and exciting experience. There are a variety of different jobs available at festivals, from setting up the event grounds to working in the ticket booth or
-            selling merchandise. You could also be a stage manager, helping to move equipment and manage the performers. There are also opportunities to work in food service, providing meals for festival goers, or to help out with security and crowd
-            control. No matter what job you take on, working at a festival is sure to be a rewarding and unforgettable experience. If you are interested in a job at FooFest send your CV and application to{" "}
-            <a href=" mailto: jobs@foofest.com ">jobs@foofest.com </a>.
-          </p>
+    <section id="work">
+      <div className="work-container">
+        <div className="text-wrapper">
+          <div className="work-text">
+            <h1>WORK AT FOOFEST</h1>
+            <p>
+              Working at a festival is a great way to be part of the action and have a unique and exciting experience.
+              There are a variety of different jobs available at festivals, from setting up the event grounds to working
+              in the ticket booth or selling merchandise. You could also be a stage manager, helping to move equipment
+              and manage the performers. There are also opportunities to work in food service, providing meals for
+              festival goers, or to help out with security and crowd control. No matter what job you take on, working at
+              a festival is sure to be a rewarding and unforgettable experience. If you are interested in a job at
+              FooFest send your CV and application to <a href="#">jobs@foofest.com </a>.
+            </p>
+          </div>
         </div>
-        <div className="aboutImage aboutImage1"></div>
+        <div className="work-image"></div>
       </div>
     </section>
   );
 }
 
-export default Job
+export default Job;
--- src/components/jobs/Voluenteer.jsx
+++ src/components/jobs/Voluenteer.jsx
@@ -1,22 +1,27 @@
-import React from 'react'
+import React from "react";
 
 function Voluenteer() {
   return (
-    <section id="job-sec">
-      <div className="about-container">
-        <div className="aboutImage aboutImage2"></div>
-        <div className="aboutText">
-          <h2>VOLUNTEER AT FOOFEST</h2>
-          <p>
-            Volunteering at a festival is a great way to attend the event for free and be part of the action! As a volunteer, you will be able to help with a variety of tasks, from setting up the event grounds to working at the information booth or
-            selling merchandise. You'll get to meet new people and be part of a dynamic and exciting atmosphere. Plus, you'll get a free ticket to the festival and access to special perks like discounts on food and drinks! If you're looking for an
-            opportunity to give back and have a great time, consider volunteering at FooFest – it's sure to be an unforgettable experience! If you want to volunteer at Foofest send your CV and application to{" "}
-            <a href=" mailto: volunteer@foofest.com">volunteer@foofest.com</a>.
-          </p>
+    <section id="volunteer">
+      <div className="volunteer-container">
+        <div className="text-wrapper">
+          <div className="volunteer-text">
+            <h2>VOLUNTEER AT FOOFEST</h2>
+            <p>
+              Volunteering at a festival is a great way to attend the event for free and be part of the action! As a
+              volunteer, you will be able to help with a variety of tasks, from setting up the event grounds to working
+              at the information booth or selling merchandise. You'll get to meet new people and be part of a dynamic
+              and exciting atmosphere. Plus, you'll get a free ticket to the festival and access to special perks like
+              discounts on food and drinks! If you're looking for an opportunity to give back and have a great time,
+              consider volunteering at FooFest – it's sure to be an unforgettable experience! If you want to volunteer
+              at Foofest send your CV and application to <a href="#">volunteer@foofest.com</a>.
+            </p>
+          </div>
         </div>
+        <div className="volunteer-image"></div>
       </div>
     </section>
   );
 }
 
-export default Voluenteer
+export default Voluenteer;
