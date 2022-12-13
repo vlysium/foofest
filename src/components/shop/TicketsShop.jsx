e147d21297ad5c09460086c4be12e382cb9e3689
Victor Ly <vict4222@stud.kea.dk>
2022-12-13 10:57:13 +0100

    styling and content

--- src/components/shop/TicketsShop.jsx
+++ src/components/shop/TicketsShop.jsx
@@ -3,9 +3,11 @@ import TicketCard from "./TicketCard";
 
 function TicketsShop() {
   const card = {
-    regular: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quaerat minima inventore.",
-    vip: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quaerat minima inventore.",
-    camping: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quaerat minima inventore.",
+    regular:
+      "This ticket gives you access to the festival on the chosen day. With your ticket, you will be able to attend the main stage events, explore the food, drinks, and vendor village!",
+    vip: "This exclusive VIP ticket gives you access to the most exclusive parts of the festival! Enjoy a day of luxury with unlimited food and drinks, exclusive artist meet and greets, and a private viewing area with the best view of the stage!",
+    camping:
+      "Our team will arrive at your designated camping area with a pre-pitched tent that has been set up in advance. All you need to do is show up and be ready to enjoy the festival!",
   };
 
   return (
@@ -13,9 +15,30 @@ function TicketsShop() {
       <h2>FOOFEST TICKETS</h2>
       <div id="shop-wrapper">
         <ul id="shop-layout">
-          <TicketCard title="VIP" text={card.vip} price="1299,-" buttonText="BUY NOW" link="tickets" className="ticket-card ticket-vip highlighted" />
-          <TicketCard title="Regular" text={card.regular} price="799,-" buttonText="BUY NOW" link="tickets" className="ticket-card ticket-regular" />
-          <TicketCard title="Camping" text={card.camping} price="99,-" buttonText="LEARN MORE" link="camping" className="ticket-card ticket-camping" />
+          <TicketCard
+            title="VIP"
+            text={card.vip}
+            price="1299,-"
+            buttonText="BUY NOW"
+            link="tickets"
+            className="ticket-card ticket-vip highlighted"
+          />
+          <TicketCard
+            title="Regular"
+            text={card.regular}
+            price="799,-"
+            buttonText="BUY NOW"
+            link="tickets"
+            className="ticket-card ticket-regular"
+          />
+          <TicketCard
+            title="Camping"
+            text={card.camping}
+            price="99,-"
+            buttonText="LEARN MORE"
+            link="camping"
+            className="ticket-card ticket-camping"
+          />
         </ul>
       </div>
     </section>
